8cfb7e0ee14f94ad2bd04853c79caf8aecfa830b
m.maddes <m.maddes@NBCH01450.lombardi.ch>
2023-05-05 16:32:25 +0200

    .

--- src/main/java/ch/lombardi/spai/commons/notifications/Action.java
+++ src/main/java/ch/lombardi/spai/commons/notifications/Action.java
@@ -3,9 +3,9 @@ package ch.lombardi.spai.commons.notifications;
 public enum Action {
 
 	VISTO,
-
 	APPROVAZIONE,
-
+	APPROVAZIONE_CS,
+	APPROVAZIONE_AMM,
 	VERIFICA
 
 	// aggiungere eventuali azioni mancanti
