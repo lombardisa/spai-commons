af1f4c27720a529b3034bc5a5f1e4749975c5f6e
m.maddes <m.maddes@NBCH01450.lombardi.ch>
2023-07-03 16:32:33 +0200

    E-REC: mega API update - PART 2

--- src/main/java/ch/lombardi/spai/commons/rec/AnnuncioEsperienzaImpl.java
+++ src/main/java/ch/lombardi/spai/commons/rec/AnnuncioEsperienzaImpl.java
@@ -14,11 +14,16 @@ public class AnnuncioEsperienzaImpl implements AnnuncioEsperienza {
 	 * UID: technical internal identification number (serialization).
 	 */
 	private static final long serialVersionUID = 2676876379619731666L;
+	
+	private final Integer STATUS_OK = 200;
+	private final Integer STATUS_ERROR = 404;
 
 	// VARIABLES
+	private Integer status;
 	private Integer annuncioId;
 	private Integer esperienzaId;
 	private String desc;
+	private String errorDesc;
 
 	/**
 	 * Default empty constructor.
@@ -26,7 +31,7 @@ public class AnnuncioEsperienzaImpl implements AnnuncioEsperienza {
 	public AnnuncioEsperienzaImpl() {
 		super();
 	}
-
+	
 	/**
 	 * Costructor of the experience needed for an annuncio.
 	 * 
@@ -37,9 +42,53 @@ public class AnnuncioEsperienzaImpl implements AnnuncioEsperienza {
 	 */
 	public AnnuncioEsperienzaImpl(Integer annuncioId, Integer esperienzaId, String desc) {
 		super();
+		this.status = STATUS_OK;
 		this.annuncioId = annuncioId;
 		this.esperienzaId = esperienzaId;
 		this.desc = desc;
+	}
+
+	/**
+	 * Method for the error. It uses two Strings.
+	 * 
+	 * @param errorCode: code of the error.
+	 * @param errorDesc: description of the error.
+	 */
+	public AnnuncioEsperienzaImpl(String errorDesc) {
+		this.status = STATUS_ERROR;
+		this.errorDesc = errorDesc;
+	}
+
+	/**
+	 * String for the Error Code
+	 */
+	public Integer getStatus() {
+		return status;
+	}
+
+	/**
+	 * Sets the Error Code.
+	 * 
+	 * @param Error Code.
+	 */
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+	/**
+	 * String for the Error Desc
+	 */
+	public String getErrorDesc() {
+		return errorDesc;
+	}
+
+	/**
+	 * Sets the Error Desc.
+	 * 
+	 * @param Error Desc.
+	 */
+	public void setErrorDesc(String errorDesc) {
+		this.errorDesc = errorDesc;
 	}
 
 	/**
