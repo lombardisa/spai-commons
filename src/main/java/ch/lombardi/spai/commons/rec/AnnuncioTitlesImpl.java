73c36663e9163a542ca7499e130260aaf3c80511
Matteo Maddes <matteo.maddes@lombardi.group>
2024-01-02 17:28:57 +0100

    Added: AnnuncioTitlesImpl

--- src/main/java/ch/lombardi/spai/commons/rec/AnnuncioTitlesImpl.java
+++ src/main/java/ch/lombardi/spai/commons/rec/AnnuncioTitlesImpl.java
@@ -8,9 +8,11 @@ public class AnnuncioTitlesImpl implements AnnuncioTitles {
 	 * UID: technical internal identification number (serialization).
 	 */
 	private static final long serialVersionUID = -4600029025301335168L;
-	
+
 	// VARIABLES
 	private Integer annuncioId;
+	private Integer sedeId;
+	private Integer sezioneId;
 	private String titolo;
 
 	/**
@@ -20,9 +22,11 @@ public class AnnuncioTitlesImpl implements AnnuncioTitles {
 		super();
 	}
 
-	public AnnuncioTitlesImpl(Integer annuncioId, String titolo) {
+	public AnnuncioTitlesImpl(Integer annuncioId, Integer sedeId, Integer sezioneId, String titolo) {
 		super();
 		this.annuncioId = annuncioId;
+		this.sedeId = sedeId;
+		this.sezioneId = sezioneId;
 		this.titolo = titolo;
 	}
 
@@ -30,6 +34,20 @@ public class AnnuncioTitlesImpl implements AnnuncioTitles {
 	 * Gets the ID_ANNUNCIO.
 	 */
 	public Integer getAnnuncioId() {
+		return annuncioId;
+	}
+
+	/**
+	 * Gets the ID_SEDE.
+	 */
+	public Integer getSedeId() {
+		return annuncioId;
+	}
+
+	/**
+	 * Gets the ID_SEZIONE.
+	 */
+	public Integer getSezioneId() {
 		return annuncioId;
 	}
 
@@ -50,6 +68,24 @@ public class AnnuncioTitlesImpl implements AnnuncioTitles {
 	}
 
 	/**
+	 * Sets the ID_SEDE.
+	 * 
+	 * @param annuncioId: the ID_SEDE.
+	 */
+	public void setSedeId(Integer sedeId) {
+		this.sedeId = sedeId;
+	}
+
+	/**
+	 * Sets the ID_SEZIONE.
+	 * 
+	 * @param annuncioId: the ID_SEZIONE.
+	 */
+	public void setSezioneId(Integer sezioneId) {
+		this.sezioneId = sezioneId;
+	}
+
+	/**
 	 * Sets the TITOLO.
 	 * 
 	 * @param titolo: the TITOLO.
@@ -60,7 +96,7 @@ public class AnnuncioTitlesImpl implements AnnuncioTitles {
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(annuncioId, titolo);
+		return Objects.hash(annuncioId, sedeId, sezioneId, titolo);
 	}
 
 	@Override
@@ -72,6 +108,8 @@ public class AnnuncioTitlesImpl implements AnnuncioTitles {
 		if (getClass() != obj.getClass())
 			return false;
 		AnnuncioTitlesImpl other = (AnnuncioTitlesImpl) obj;
-		return Objects.equals(annuncioId, other.annuncioId) && Objects.equals(titolo, other.titolo);
+		return Objects.equals(annuncioId, other.annuncioId) && Objects.equals(sedeId, other.sedeId)
+				&& Objects.equals(sezioneId, other.sezioneId) && Objects.equals(titolo, other.titolo);
 	}
+
 }
