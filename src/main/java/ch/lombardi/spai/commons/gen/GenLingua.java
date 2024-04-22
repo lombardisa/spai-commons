605cf519ba65a9c0339f2cb5507bf9354a0e4209
Matteo Maddes <matteo.maddes@lombardi.group>
2024-04-22 16:35:03 +0200

    NEW: GenLingua, GenLinguaImpl.

---
+++ src/main/java/ch/lombardi/spai/commons/gen/GenLingua.java
@@ -0,0 +1,29 @@
+package ch.lombardi.spai.commons.gen;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+
+import ch.lombardi.spai.commons.rec.LinguaImpl;
+
+/**
+ * Java Interface: Lingua.java
+ * 
+ * @author Matteo Maddes
+ * @version 22.04.2024
+ */
+@JsonDeserialize(as = LinguaImpl.class)
+public interface GenLingua {
+
+	/**
+	 * Returns the ID_CHIAVE.
+	 * 
+	 * @return: the ID_CHIAVE.
+	 */
+	String getChiaveId();
+
+	/**
+	 * Returns the TESTO of the ID_CHIAVE.
+	 * 
+	 * @return: the TESTO of the ID_CHIAVE.
+	 */
+	String getTesto();
+}
